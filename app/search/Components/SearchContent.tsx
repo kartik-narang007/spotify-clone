0385bc966d99be4ab6aa7c35adcc3928bc327347
Kartik Narang <narangk007@gmail.com>
2024-04-14 17:55:35 +0530

    Added Like Feature in App

--- app/search/Components/SearchContent.tsx
+++ app/search/Components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
@@ -54,7 +55,11 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
                 data={song}
                 />
             </div>
-            {/* TODO: add like button here */}
+
+
+            <LikeButton songId={song.id}/>
+
+
         </div>
       ))}
     </div>
